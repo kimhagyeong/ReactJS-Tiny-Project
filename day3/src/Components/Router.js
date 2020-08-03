47078fe2071c5b1aaa4413b61a36e0fa4ab235d5
jonghopark95 <https://github.com/jonghopark95>
2020-08-04 01:24:14 +0900

    day3 clear

--- day3/src/Components/Router.js
+++ day3/src/Components/Router.js
@@ -1,5 +1,19 @@
 import React from "react";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Prices from "../Screens/Prices";
+import Exchanges from "../Screens/Exchanges";
+import Coins from "../Screens/Coins";
+import Header from "./Header";
 
 export default () => {
-  return <Router>{/* Magic Here */}</Router>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Prices}></Route>
+        <Route exact path="/exchanges" component={Exchanges}></Route>
+        <Route exact path="/coins" component={Coins}></Route>
+      </Switch>
+    </Router>
+  );
+};
