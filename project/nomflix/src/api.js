61a64c17e2592fbce514d244dfd730fed570cfae
jonghopark95 <https://github.com/jonghopark95>
2020-08-06 02:55:46 +0900

    ~~~5.1

---
+++ project/nomflix/src/api.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const my_api_key = "8cae33bc12bc110ba6ee595c1cf2abe1";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: `${my_api_key}`,
+    language: "en-US",
+  },
+});
+
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  movieDetail: (id) =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get("search/movie", {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  showDetail: (id) =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get("search/tv", {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
+};
